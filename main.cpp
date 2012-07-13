7ed237978791af2f3b1b08549ee3bb5af89e0ede
SMHRambo <SMHRambo@googlemail.com>
2012-07-14 01:49:47 +0200

    Add ProjectFiles

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: sascha
+ *
+ * Created on 14. Juli 2012, 01:44
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
